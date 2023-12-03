d453d07cf550e97ed8ca2616ca7c99f2e34a71ce
florian121212 <64419975+florian121212@users.noreply.github.com>
2023-12-03 11:49:16 +0100

    cam position

--- airport_capstone/Assets/Scripts/screenshots.cs
+++ airport_capstone/Assets/Scripts/screenshots.cs
@@ -29,7 +29,7 @@ public class screenshots : MonoBehaviour
         while (true)
         {
             takePics();
-            yield return new WaitForSeconds(0.2f);
+            yield return new WaitForSeconds(0.5f);
         }
     }
 
